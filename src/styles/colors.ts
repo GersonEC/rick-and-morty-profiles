3fb22f645228a60e877a8d869b8c02c0b6fa9ab3
GersonEC <gersonenriquez94@gmail.com>
2021-07-02 12:10:58 +0200

    Clean App. Add folder structure.

---
+++ src/styles/colors.ts
@@ -0,0 +1,43 @@
+const white = "white";
+const black = "black";
+const text = "#434449";
+const gray = "#f1f2f7";
+const gray5 = "#f1f2f7";
+const gray10 = "#f1f1f4";
+const gray20 = "#e4e5e9";
+const darkgray = "#221E21";
+const indigo = "#3f51b5";
+const indigoDarken10 = "#364495";
+const indigoLighten80 = "#b7c1f8";
+const yellow = "yellow";
+const green = "#4caf50";
+const danger = "#ef5350";
+const orange = "orange";
+const beige = "#AD9A77";
+const transparent = "transparent";
+const blue = "dodgerblue";
+const lightBlue = "deepskyblue";
+const red = "red";
+
+export {
+  red,
+  green,
+  white,
+  black,
+  lightBlue,
+  blue,
+  transparent,
+  orange,
+  beige,
+  danger,
+  text,
+  gray,
+  gray5,
+  gray10,
+  gray20,
+  darkgray,
+  indigo,
+  indigoDarken10,
+  indigoLighten80,
+  yellow,
+};
---
+++ src/styles/media-queries.ts
@@ -0,0 +1,4 @@
+export const large = "@media (min-width: 1200px)";
+export const medium = "@media (min-width: 769px) and (max-width: 1199px)";
+export const small = "@media (max-width: 768px)";
+export const mobile = "@media (max-width: 375px)";

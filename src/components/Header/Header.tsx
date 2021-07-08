e9409400ca91bd86bebb12cb99b3c59003fbc17c
GersonEC <gersonenriquez94@gmail.com>
2021-07-08 10:53:00 +0200

    Code Refactor 2

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,18 +3,9 @@ import { Nav } from "./Header.style";
 import RickAndMortyLogo from "../../assets/images/rick_and_morty_logo.svg";
 
 export default function Header(): JSX.Element {
-  const onLogoClick = () => {
-    window.scroll(0, 0);
-  };
-
   return (
     <Nav>
-      <img
-        src={RickAndMortyLogo}
-        width={200}
-        alt="rick and morty"
-        onClick={onLogoClick}
-      />
+      <img src={RickAndMortyLogo} width={200} alt="rick and morty" />
     </Nav>
   );
 }
